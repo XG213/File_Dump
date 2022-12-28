279214c5ec0ce26ec090680a0971ce861dce0015
Idiot <101302347+XG213@users.noreply.github.com>
2022-12-28 19:47:08 +1300

    Create Map_Seat.lua

---
+++ Map_Seat.lua
@@ -0,0 +1,15 @@
+--ss fix for seat
+local seat = Instance.new("Seat")
+seat.Name = "Seat"
+seat.Anchored = true
+seat.BrickColor = BrickColor.new("Brown")
+seat.CFrame = CFrame.new(6.39965343, 3.2000072, 31.100132, -9.68575478e-08, 0, -1, 0, 1, 0, 1, 0, -9.68575478e-08)
+seat.Color = Color3.fromRGB(124, 92, 70)
+seat.Material = Enum.Material.Wood
+seat.Orientation = Vector3.new(0, -90, 0)
+seat.Position = Vector3.new(6.4, 3.2, 31.1)
+seat.Rotation = Vector3.new(0, -90, 0)
+seat.Size = Vector3.new(1.8, 0.2, 1)
+seat.TopSurface = Enum.SurfaceType.SmoothNoOutlines
+seat.Transparency = 1
+seat.Parent = chair
